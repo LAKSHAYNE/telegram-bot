7ee9289cb6bc75db69c2e7b6880be64cdcf1a9af
LAKSHAY SHARMA <lakshayne@gmail.com>
2020-11-09 20:12:28 +0530

    day1.1 commit

--- app.js
+++ app.js
@@ -6,32 +6,8 @@ const token = '1480794225:AAF1aNUEiBJ2TEfjkMJ_BHJwR4aoxC7eSG4';
 // Create a bot that uses 'polling' to fetch new updates
 const bot = new TelegramBot(token, {polling: true});
  
-// // Matches "/echo [whatever]"
-// bot.onText(/\/echo (.+)/, (msg, match) => {
-//   // 'msg' is the received Message from Telegram
-//   // 'match' is the result of executing the regexp above on the text content
-//   // of the message
- 
-//   const chatId = msg.chat.id;
-//   const resp = match[1]; // the captured "whatever"
- 
-//   // send back the matched "whatever" to the chat
-//   bot.sendMessage(chatId, resp);
-// });
-
-// bot.on("message",(msg)=>{
-//     const chatId=msg.chat.id;
-//     if(msg.text.toString()==="who are u"){
-//         bot.sendMessage(chatId,"I am a mufukin starbot");
-//     }
-//     else{
-//         bot.once("location",(msg)=>{
-//             var latit=msg.location.latitude;
-//             console.log(latit);
-//             bot.sendMessage(chatId,latit);
-//         });    
-//     }
-// });
-
-
-
+bot.on("message",(msg)=>{
+    const chatId=msg.chat.id;
+    console.log(msg.from.username);
+    bot.sendMessage(chatId,"kjsdkladlkajdljla");
+});
